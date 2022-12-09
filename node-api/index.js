ae00df7c8e9775ecc3bc4be11b74214de4cb4ffa
Chisomchima <87203527+Chisomchima@users.noreply.github.com>
2022-12-09 23:54:01 +0100

    added the routs and error handler

--- node-api/index.js
+++ node-api/index.js
@@ -5,6 +5,8 @@ import authRoute from './routes/auth.js'
 import hotelRoute from './routes/auth.js'
 import roomsRoute from './routes/auth.js'
 import usersRoute from './routes/auth.js'
+import cookieParser from "cookie-parser"
+import cors from "cors";
 
 const app = express();
 dotenv.config();
@@ -22,12 +24,26 @@ mongoose.connection.on('disconnected', () => {
 })
 
 //middlewares
+app.use(cors())
+app.use(cookieParser())
+app.use(express.json());
+
 app.use('/api/auth', authRoute);
 app.use('/api/users', usersRoute);
 app.use('/api/hotels', hotelRoute);
 app.use('/api/rooms', roomsRoute);
 
-
+//error handling
+app.use((err, req, res, next) => {
+    const errorStatus = err.status || 500;
+    const errorMessage = err.message || "Something went wrong!";
+    return res.status(errorStatus).json({
+      success: false,
+      status: errorStatus,
+      message: errorMessage,
+      stack: err.stack,
+    });
+  });
 
 app.listen('8080', ()=>{
     connect()
